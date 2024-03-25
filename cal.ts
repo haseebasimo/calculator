958010eeb7137caa174d7d51e7bf8b78c2a05f64
haseebasimo <160479535+haseebasimo@users.noreply.github.com>
2024-03-26 00:26:53 +0500

    Add files via upload

---
+++ cal.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer"
+
+const answer =  await  inquirer.prompt([
+    {message:"Enter first number",type:"number",name:"FirstNumber"},
+    {message:"Enter second number",type:"number",name:"secondNumber"},   
+    { message:"Select one of the operator to perform operation",
+    type:"list",
+    name:"operator",
+    choices:["addition","Subtraction","Multiplication","Division"],
+},
+])
+
+if (answer.operator ==="addition") {
+    console.log(answer.FirstNumber+answer.secondNumber)
+} else if (answer.operator==="subtraction") {
+  console.log(answer.FirstNumber-answer.secondNumber)
+} else if (answer.operator==="Multiplication"){
+   console.log(answer.FirstNumber*answer.secondNumber)
+} else if (answer.operator==="Division"){
+    console.log(answer.FirstNumber/answer.secondNumber)
+} else{
+    console.log("Please slect valid number")
+}
+
+
+//
+
